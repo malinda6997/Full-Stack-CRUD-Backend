98d78bfa1d481dddbe3963195147031c988f4a7b
Malinda Prabath <malindaprabath876@gmail.com>
2025-05-27 12:04:09 +0530

    set up a post method

--- main.py
+++ main.py
@@ -9,6 +9,32 @@ def get_contacts():
     json_contacts = list(map(lambda contact: contact.to_json(), contactcts))
     return jsonify({"contacts":json_contacts}), 200
 
+@app.route('/contacts', methods=['POST'])
+def create_contact():
+    first_name = request.json.get('firstName')
+    last_name = request.json.get('lastName')
+    mobile_number = request.json.get('mobileNumber')
+    age = request.json.get('age')
+    
+    if not first_name or not last_name or not mobile_number or not age:
+        return (
+            jsonify({"message": "All fields are required"}), 400
+            )
+    new_contact = Contact(
+        first_name=first_name,
+        last_name=last_name,
+        mobile_number=mobile_number,
+        age=age
+    )
+    try:
+        db.session.add(new_contact)
+        db.session.commit()
+        return jsonify({"message": "Contact created successfully"}), 201
+    except Exception as e:
+        return (
+            jsonify({"message": "An error occurred while creating the contact", "error": str(e)}), 400
+        )
+    
 
 
 if __name__ == '__main__':
